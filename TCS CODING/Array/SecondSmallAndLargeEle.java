d64092483d25e73d202044a86df5f2125aa44169
yashyk123 <91341397+yashyk123@users.noreply.github.com>
2025-02-14 00:18:11 +0530

    add new  ByusingCondition function

--- TCS CODING/Array/SecondSmallAndLargeEle.java
+++ TCS CODING/Array/SecondSmallAndLargeEle.java
@@ -15,11 +15,47 @@ public class SecondSmallAndLargeEle {
         int secSmall = a[1];
         return secSmall;
     }
+
+
+    public int[] ByusingCondition(int arr[])
+    {
+        if(arr.length==0 || arr.length==1)
+        {
+            return new int[] {-1,-1};
+        }
+
+        int small = Integer.MAX_VALUE;
+        int secSmall = Integer.MAX_VALUE;
+        int large = Integer.MIN_VALUE;
+        int secLarge = Integer.MIN_VALUE;
+
+        for(int i=0;i < arr.length;i++)
+        {
+            small = Math.min(small, arr[i]);
+            large = Math.max(large, arr[i]);
+        }
+
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i] < secSmall && arr[i]!=small)
+            {
+                secSmall = arr[i];
+            }
+            if(arr[i] > secLarge && arr[i]!=large)
+            {
+                secLarge = arr[i];
+            }
+        }
+
+        return new int[] {secSmall, secLarge};
+    }
     public static void main(String[] args) {
         int arr[] ={1,2,4,7,7,5};
         SecondSmallAndLargeEle obj = new SecondSmallAndLargeEle();
         System.out.println("Second largest element is: "+obj.FindSecLarge(arr));
         System.out.println("Second smallest element is: "+obj.FindSecSmall(arr));
+
+        System.out.println("Second smallest and Second largest element is: "+ Arrays.toString(obj.ByusingCondition(arr)));
 
     }
 }
